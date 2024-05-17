1c6b7a0d6272ecc34ffe889ae3d04d17871f8d20
farhanrdhani <farhanrdhani11@gmail.com>
2024-05-17 21:32:43 +0700

    Initial Commit

---
+++ PassThePasta/PassThePastaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PassThePastaApp.swift
+//  PassThePasta
+//
+//  Created by Farhan Rizqy Ramadhani on 17/05/24.
+//
+
+import SwiftUI
+
+@main
+struct PassThePastaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
